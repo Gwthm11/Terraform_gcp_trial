f57a75b8b53084c4af51cfd966591b4b4174bb3f
Gwthm11 <88527791+Gwthm11@users.noreply.github.com>
2022-07-04 11:37:56 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,4 +1,8 @@
-
+import urllib 
+from urllib.request import urlopen
+html = urlopen("http://pythonscraping.com/pages/page1.html")
+contents = html.read()
+print(contents)
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0',
