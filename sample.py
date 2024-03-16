aedad3fd3f8d2349fa2be19f8ee08da1ecabacfb
Gwthm11 <88527791+Gwthm11@users.noreply.github.com>
2024-03-16 09:36:39 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,3 +1,5 @@
+curl -o cloud-sql-proxy https://storage.googleapis.com/cloud-sql-connectors/cloud-sql-proxy/v2.9.0/cloud-sql-proxy.linux.amd64
+
 import pymysql
 from google.oauth2 import service_account
 import google.auth.transport.requests
